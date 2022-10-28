92daa8bc52832f640cc6e85c94050ba6e8a953a3
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-10-28 14:04:30 +0200

    es finito

--- js/script.js
+++ js/script.js
@@ -3,9 +3,14 @@ const {createApp} = Vue;
 const app = createApp({
     data(){
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            testo: 'Oh no!',
+            active: true
         }
     },
     methods:{
+        toggle(){
+            this.active = !this.active;
+        }
     }
 }).mount('.box');
